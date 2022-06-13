13ad5803a74967b61f1d69e69c1d30c842baf80d
Dimas M <dimas.m@softwareseni.com>
2022-06-13 08:58:05 +0700

    Initialize project using Create React App

---
+++ src/core/__tests__/app.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { act, render } from '@testing-library/react'
+import App from '../app'
+import { createRoot } from 'react-dom/client'
+
+jest.mock('../router')
+
+describe('App', () => {
+  it('should render without crashing', () => {
+    const div = document.createElement('div')
+    act(() => {
+      const root = createRoot(div)
+      root.render(<App />)
+      root.unrender()
+    })
+  })
+
+  it('should match a snapshot', () => {
+    const app = render(<App />)
+    act(() => {
+      expect(app).toMatchSnapshot()
+    })
+  })
+})
